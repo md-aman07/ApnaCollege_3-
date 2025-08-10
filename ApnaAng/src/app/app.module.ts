d7fcd92780ac75ade8aabeefb9e358eb9c3e1260
md-aman07 <mohdaman21426@gmail.com>
2025-08-10 22:57:43 +0530

    add new component

--- ApnaAng/src/app/app.module.ts
+++ ApnaAng/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoutComponent } from './rout/rout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutComponent
   ],
   imports: [
     BrowserModule,
